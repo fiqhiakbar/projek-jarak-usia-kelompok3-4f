82b4b88cc15acd4098035a777b290351cf5926b7
fiqhiakbar <fiqhiakbar22@gmail.com>
2023-08-08 20:10:13 +0700

    gui

---
+++ uas/GUI/src/Period.java
@@ -0,0 +1,32 @@
+
+import java.time.LocalDate;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author fiqhi
+ */
+class Period {
+
+    static Period between(LocalDate tanggalLahir1, LocalDate tanggalLahir2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    String getYears() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    String getMonths() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    String getDays() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
