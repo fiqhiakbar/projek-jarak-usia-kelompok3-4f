b8727c5abe3ad279563908ac8cf7a86ddcbf40a9
fiqhiakbar <fiqhiakbar22@gmail.com>
2023-08-08 08:16:51 +0700

    individu

---
+++ individu/UTS_putra.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts_putra;
+
+/**
+ *
+ * @author fiqhi
+ */
+public class UTS_putra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("======================================");
+        System.out.println("Nama  : putra wahyu Nugroho");
+        System.out.println("NIM   : 2113010237");
+        System.out.println("Kelas : 4F ");
+        System.out.println("======================================");
+
+    }
+}
